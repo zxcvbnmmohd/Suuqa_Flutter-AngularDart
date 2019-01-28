f2f0e3988bf1e93dfc9f5e7d116b3b4330a06537
Mohamed Mohamed <studio@mohd.ca>
2019-01-28 14:05:22 -0500

    Second Commit

--- suuqa_firebase/functions/src/index.ts
+++ suuqa_firebase/functions/src/index.ts
@@ -1,8 +1,57 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as algoliaSearch from "algoliasearch";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const config = functions.config();
+const algoliaClient = algoliaSearch(
+  config.algolia.appid,
+  config.algolia.adminapikey
+);
+const productsIndex = algoliaClient.initIndex("products");
+const usersIndex = algoliaClient.initIndex("users");
+
+exports.createProduct = functions.firestore
+  .document("products/{pID}")
+  .onCreate((snap, context) => {
+    const objectID = snap.id;
+    const data = snap.data();
+    const title = data.title;
+    const _geoloc = {
+      lat: data.address.geoPoint._latitude,
+      lng: data.address.geoPoint._longitude
+    };
+
+    return productsIndex.addObject({
+      objectID,
+      title,
+      _geoloc
+    });
+  });
+
+exports.deleteProduct = functions.firestore
+  .document("products/{pID}")
+  .onDelete((snap, context) => {
+    const objectID = snap.id;
+
+    return productsIndex.deleteObject(objectID);
+  });
+
+exports.createUser = functions.firestore
+  .document("users/{uID}")
+  .onCreate((snap, context) => {
+    const objectID = snap.id;
+    const data = snap.data();
+    const _geoloc = { lat: 1.0, lng: 1.0 };
+
+    return usersIndex.addObject({ objectID, data });
+  });
+
+exports.deleteUser = functions.firestore
+  .document("users/{uID}")
+  .onDelete((snap, context) => {
+    const objectID = snap.id;
+
+    return usersIndex.deleteObject(objectID);
+  });
